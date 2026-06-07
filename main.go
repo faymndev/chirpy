81bf4ce3a72c3b99d0899f67aace828130e76c90
faymndev <faymndev@gmail.com>
2026-06-07 08:20:53 -0700

    logging middleware

--- main.go
+++ main.go
@@ -2,21 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
+func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/healthz", middlewareLog(healthHandler{}))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("public"))))
+	server := &http.Server{Addr: ":8080", Handler: mux}
+	server.ListenAndServe()
+}
+
 type healthHandler struct{}
 
-func (h healthHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+func (h healthHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain")
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprint(w, "OK")
 }
 
-func main() {
-	mux := http.NewServeMux()
-	mux.Handle("/healthz", healthHandler{})
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("public"))))
-	server := &http.Server{Addr: ":8080", Handler: mux}
-	server.ListenAndServe()
+func middlewareLog(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s\n", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
 }
