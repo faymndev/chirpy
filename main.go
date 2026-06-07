7a6960db8e0a711aa712011cdc2f98c5fade72dd
faymndev <faymndev@gmail.com>
2026-06-07 08:41:21 -0700

    metrics middleware

--- main.go
+++ main.go
@@ -4,14 +4,47 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
 func main() {
+	cfg := NewApiConfig()
+
 	mux := http.NewServeMux()
-	mux.Handle("/healthz", middlewareLog(healthHandler{}))
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("public"))))
+	mux.Handle("GET /healthz", middlewareLog(healthHandler{}))
+	mux.Handle("/app/", cfg.middlewareMetrics(http.StripPrefix("/app", http.FileServer(http.Dir("public")))))
+
+	mux.HandleFunc("GET /metrics", cfg.handleMetrics)
+	mux.HandleFunc("POST /reset", cfg.handleReset)
+
 	server := &http.Server{Addr: ":8080", Handler: mux}
 	server.ListenAndServe()
+}
+
+type apiConfig struct {
+	hits atomic.Int32
+}
+
+func NewApiConfig() *apiConfig {
+	return &apiConfig{}
+}
+
+func (cfg *apiConfig) middlewareMetrics(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.hits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hits: %d", cfg.hits.Load())
+}
+
+func (cfg *apiConfig) handleReset(w http.ResponseWriter, r *http.Request) {
+	cfg.hits.Store(0)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hits: 0")
 }
 
 type healthHandler struct{}
